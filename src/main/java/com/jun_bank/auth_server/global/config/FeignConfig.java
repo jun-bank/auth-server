dd4d3507929ddc3afdebe6086ae309aafc28de92
jun <pickjog@gmail.com>
2025-11-30 18:07:45 +0900

    feat(global-config): FeignConfig 추가

---
+++ src/main/java/com/jun_bank/auth_server/global/config/FeignConfig.java
@@ -0,0 +1,41 @@
+package com.jun_bank.auth_server.global.config;
+
+import com.jun_bank.auth_server.global.feign.FeignErrorDecoder;
+import com.jun_bank.auth_server.global.feign.FeignRequestInterceptor;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feign Client 설정
+ */
+@Configuration
+@EnableFeignClients(basePackages = "com.jun_bank.auth_server")
+public class FeignConfig {
+
+    /**
+     * Feign 로깅 레벨
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+    /**
+     * Feign 에러 디코더
+     */
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new FeignErrorDecoder();
+    }
+
+    /**
+     * Feign 요청 인터셉터 (인증 헤더 전파)
+     */
+    @Bean
+    public FeignRequestInterceptor feignRequestInterceptor() {
+        return new FeignRequestInterceptor();
+    }
+}
